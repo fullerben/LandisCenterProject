be15a8c9c915b0e4caa17379239391d27147ff50
Ben Fuller <fullerb@lafayette.edu>
2019-11-25 10:37:40 -0500

    added forms and body parser middleware

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.engine('html', ejs.renderFile)
 app.use(express.static(path.join(__dirname, '/client')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.use('/', index)
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,13 @@ router.get('/all', async (req, res) => {
     res.render('index', { contacts: contacts.rows })
 })
 
+router.get('/add', (req, res) => {
+    res.render('addcontact')
+})
+
+router.post('/add', (req, res) => {
+    console.log(req.body)
+    res.redirect('/contacts/all')
+})
+
 module.exports = router
