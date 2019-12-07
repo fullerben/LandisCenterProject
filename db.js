85f57c62f92c964fc3f604784c0186223c61191f
Ben Fuller <fullerb@lafayette.edu>
2019-12-07 12:42:08 -0500

    made queries not case sensitive

--- db.js
+++ db.js
@@ -52,7 +52,7 @@ module.exports = {
     },
     getFacultyContactUsingDepartment: (department) => {
         let likename = '%' + department + '%';
-    	return queryTemplate('select contacts.email, contacts.name, facultycontacts.department from facultycontacts, contacts where contacts.email = facultycontacts.email AND facultycontacts.department LIKE $1', [likename])
+    	return queryTemplate('select contacts.email, contacts.name, facultycontacts.department from facultycontacts, contacts where contacts.email = facultycontacts.email AND facultycontacts.department ILIKE $1', [likename])
     },
     getContactsUsingOrganization: (org) => {
     	return queryTemplate('select * from contacts join organizationcontacts on contacts.email=organizationcontacts.contact_email where organization = $1', [org])
@@ -62,14 +62,14 @@ module.exports = {
     },
     getOrgUsingLikeOrganization: (org) => {
         let likename = '%' + org + '%';
-    	return queryTemplate('select * from organizations where organization_name LIKE $1 order by organization_name', [likename])
+    	return queryTemplate('select * from organizations where organization_name ILIKE $1 order by organization_name', [likename])
     },
     getContactsUsingLIKEName: (name) => {
         return queryTemplate(`select * from contacts join organizationcontacts on contacts.email=organizationcontacts.contact_email where name ILIKE '%${name}%'`)
     },
     getContactUsingLikeName: (name) => {
         let likename = '%' + name + '%';
-    	return queryTemplate("select * from contacts where name LIKE $1 order by name", [likename])
+    	return queryTemplate("select * from contacts where name ILIKE $1 order by name", [likename])
     },
     getContactsUsingProject: (name) => {
     	return queryTemplate('select * from contacts join projects on contacts.email=projects.contact_email where project_name = $1', [name])
