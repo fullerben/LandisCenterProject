1fbc40ea286472aee0d099efa4d63e63d6aa1fb2
fawcettm <fawcettm@lafayette.edu>
2019-12-10 20:15:02 -0500

    molly

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const passport = require('passport')
 const authenticateUser = (req, res, next) => {
     // if(req.user) return next()
     // res.status(404).send('Please login to continue.')
-    return next()
+    return next();
 }
 
 module.exports = { authenticateUser }
